43e6ba2965b8d4b82bfe4deeaf78ccd9a7ad8ec3
Ricardo Filipe Martins Querido <ricardo.querido98@ua.pt>
2019-04-13 16:57:06 +0100

    Design started

--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class TVShow(models.Model):
+    tv_id = models.IntegerField
+    is_checked = models.BooleanField
+
+    def __str__(self):
+        return self.tv_id
+
+
+class Season(models.Model):
+    number = models.IntegerField
+    is_checked = models.BooleanField
+    tv_show = models.ForeignKey(TVShow, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
+
+
+class Episode(models.Model):
+    number = models.IntegerField
+    is_checked = models.BooleanField
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.number
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(TVShow)
+
+    def __str__(self):
+        return self.user.username
