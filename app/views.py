d9eef6537ffd01d2f794d013b683aeddc1c8fc60
Ricardo Filipe Martins Querido <ricardo.querido98@ua.pt>
2019-04-13 23:11:19 +0100

    home page almost finished

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'layout.html')
+    resp = requests.get(
+        'https://api.themoviedb.org/3/tv/popular?api_key=2bbd9e8844b9e73b0fb2e47e0f6d3b88&language=en-US&page=1')
+    if resp.status_code != 200:
+        print("SOMETHING WENT WRONG!")
+    shows = resp.json()["results"][:3]
+    return render(request, 'index.html', {'shows': shows})
